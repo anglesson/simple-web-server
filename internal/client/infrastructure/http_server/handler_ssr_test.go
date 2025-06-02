c63a9d875ece43284e3b584441e7859553c791a9
Anglesson Araujo <anglesson@outlook.com>
2025-06-02 14:02:13 -0300

    test: refactor CreateClientSSR tests to use testify suite and improve structure

--- internal/client/infrastructure/http_server/handler_ssr_test.go
+++ internal/client/infrastructure/http_server/handler_ssr_test.go
@@ -11,6 +11,7 @@ import (
 	common_infrastructure "github.com/anglesson/simple-web-server/internal/common/infrastructure"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 )
 
 type MockClientUseCase struct {
@@ -22,7 +23,44 @@ func (m *MockClientUseCase) CreateClient(input client_application.CreateClientIn
 	return args.Get(0).(*client_application.CreateClientOutput), args.Error(1)
 }
 
-func TestCreateClientSSR_Success(t *testing.T) {
+// func TestCreateClientSSR_Success(t *testing.T) {
+// 	expected := client_application.CreateClientInput{
+// 		Name:             "AnyName",
+// 		CPF:              "AnyCPF",
+// 		BirthDay:         "AnyDate",
+// 		Email:            "AnyEmail",
+// 		Phone:            "AnyPhone",
+// 		CreatorUserEmail: "any_user@mail.com",
+// 	}
+// 	mockUsecase := new(MockClientUseCase)
+
+// 	mockUsecase.On("CreateClient", expected).Return(&client_application.CreateClientOutput{}, nil).Once()
+
+// 	handler := NewSSRHandler(mockUsecase)
+
+// 	formData := strings.NewReader("name=AnyName&cpf=AnyCPF&birth_day=AnyDate&email=AnyEmail&phone=AnyPhone")
+// 	req := httptest.NewRequest(http.MethodPost, "/client", formData)
+// 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+// 	ctx := context.WithValue(req.Context(), common_infrastructure.LoggedUserKey, "any_user@mail.com")
+// 	req = req.WithContext(ctx)
+// 	rr := httptest.NewRecorder()
+// 	handler.CreateClientSubmit(rr, req)
+
+// 	assert.Equal(t, http.StatusSeeOther, rr.Code, "Expected status code 303 See Other for success add")
+// 	assert.Equal(t, "/client", rr.Header().Get("Location"), "Expected redirect to root path")
+// 	mockUsecase.AssertExpectations(t)
+// }
+
+type HandlerSSRSuit struct {
+	suite.Suite
+	MockClientUseCase *MockClientUseCase // Should be a interface
+}
+
+func (suite *HandlerSSRSuit) SetupTest() {
+	suite.MockClientUseCase = new(MockClientUseCase)
+}
+
+func (suite *HandlerSSRSuit) TestCreateClientSubmit() {
 	expected := client_application.CreateClientInput{
 		Name:             "AnyName",
 		CPF:              "AnyCPF",
@@ -31,11 +69,10 @@ func TestCreateClientSSR_Success(t *testing.T) {
 		Phone:            "AnyPhone",
 		CreatorUserEmail: "any_user@mail.com",
 	}
-	mockUsecase := new(MockClientUseCase)
 
-	mockUsecase.On("CreateClient", expected).Return(&client_application.CreateClientOutput{}, nil).Once()
+	suite.MockClientUseCase.On("CreateClient", expected).Return(&client_application.CreateClientOutput{}, nil).Once()
 
-	handler := NewSSRHandler(mockUsecase)
+	handler := NewSSRHandler(suite.MockClientUseCase)
 
 	formData := strings.NewReader("name=AnyName&cpf=AnyCPF&birth_day=AnyDate&email=AnyEmail&phone=AnyPhone")
 	req := httptest.NewRequest(http.MethodPost, "/client", formData)
@@ -45,7 +82,12 @@ func TestCreateClientSSR_Success(t *testing.T) {
 	rr := httptest.NewRecorder()
 	handler.CreateClientSubmit(rr, req)
 
-	assert.Equal(t, http.StatusSeeOther, rr.Code, "Expected status code 303 See Other for success add")
-	assert.Equal(t, "/client", rr.Header().Get("Location"), "Expected redirect to root path")
-	mockUsecase.AssertExpectations(t)
+	assert.Equal(suite.T(), http.StatusSeeOther, rr.Code, "Expected status code 303 See Other for success add")
+	assert.Equal(suite.T(), "/client", rr.Header().Get("Location"), "Expected redirect to root path")
+
+	suite.MockClientUseCase.AssertExpectations(suite.T())
+}
+
+func TestHandlerSSRSuit(t *testing.T) {
+	suite.Run(t, new(HandlerSSRSuit))
 }
