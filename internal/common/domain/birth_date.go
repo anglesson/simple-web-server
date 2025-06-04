5f226bd845807506a3fe7122f00bada199cfbb59
Anglesson Araujo <anglesson@outlook.com>
2025-06-03 22:01:12 -0300

    feat: add BirthDate type with validation and unit tests

---
+++ internal/common/domain/birth_date.go
@@ -0,0 +1,31 @@
+package common_domain
+
+import (
+	"fmt"
+	"time"
+)
+
+type BirthDate struct {
+	value time.Time
+}
+
+func NewBirthDate(date string) (*BirthDate, error) {
+	parsed, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return nil, fmt.Errorf("data de nascimento inválida: formato deve ser YYYY-MM-DD")
+	}
+
+	if parsed.After(time.Now()) {
+		return nil, fmt.Errorf("data de nascimento não pode ser no futuro")
+	}
+
+	return &BirthDate{value: parsed}, nil
+}
+
+func (b BirthDate) String() string {
+	return b.value.Format("2006-01-02")
+}
+
+func (b BirthDate) Value() time.Time {
+	return b.value
+}
