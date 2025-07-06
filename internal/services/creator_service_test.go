cfb00e49ac3188cc463ba1fe2e28088e1d44966f
Anglesson Araujo <anglesson@outlook.com>
2025-07-06 13:59:48 -0300

    test: add test should throw error if data not exists in receita federal

--- internal/services/creator_service_test.go
+++ internal/services/creator_service_test.go
@@ -18,6 +18,10 @@ type CreatorServiceTestSuite struct {
 	mockRFService   gov.ReceitaFederalService
 }
 
+func TestCreatorServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(CreatorServiceTestSuite))
+}
+
 func (suite *CreatorServiceTestSuite) SetupTest() {
 	suite.mockCreatorRepo = new(mocks_repo.MockCreatorRepository)
 	suite.mockRFService = new(mocks.MockRFService)
@@ -178,6 +182,7 @@ func (suite *CreatorServiceTestSuite) TestShouldThrowErrorIfCreatorHasARegister(
 
 	suite.Error(err)
 	suite.Assert().Nil(creator)
+	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).AssertCalled(suite.T(), "FindByFilter", expectedCreatorFilter)
 	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).AssertNotCalled(suite.T(), "Save", expectedCreator)
 	suite.mockRFService.(*mocks.MockRFService).AssertNotCalled(
 		suite.T(),
@@ -187,6 +192,55 @@ func (suite *CreatorServiceTestSuite) TestShouldThrowErrorIfCreatorHasARegister(
 	)
 }
 
-func TestCreatorServiceTestSuite(t *testing.T) {
-	suite.Run(t, new(CreatorServiceTestSuite))
+func (suite *CreatorServiceTestSuite) TestShouldThrowErrorIfDataNotExistsInReceitaFederal() {
+	input := services.InputCreateCreator{
+		Name:        "Valid Name",
+		BirthDate:   "2012-12-12",
+		PhoneNumber: "(12) 94567-8901",
+		Email:       "valid@mail.com",
+		CPF:         "058.997.950-77",
+	}
+
+	expectedCreator, _ := domain.NewCreator(
+		input.Name,
+		input.Email,
+		input.CPF,
+		input.PhoneNumber,
+		input.BirthDate,
+	)
+
+	expectedCreatorFilter := domain.CreatorFilter{
+		CPF: expectedCreator.CPF.Value(),
+	}
+
+	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).
+		On("FindByFilter", expectedCreatorFilter).
+		Return(expectedCreator, nil)
+
+	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).
+		On("FindCreatorByCPF", input.CPF).
+		Return(expectedCreator, nil)
+
+	suite.mockRFService.(*mocks.MockRFService).
+		On("ConsultaCPF", expectedCreator.CPF.String(), expectedCreator.Birthdate.Format("02/01/2006")).
+		Return(&gov.ReceitaFederalResponse{
+			Status: false, // Simulate data not found in Receita Federal
+		}, nil)
+
+	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).
+		On("Save", expectedCreator).
+		Return(nil)
+
+	creator, err := suite.sut.CreateCreator(input)
+
+	suite.Error(err)
+	suite.Assert().Nil(creator)
+	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).AssertCalled(suite.T(), "FindByFilter", expectedCreatorFilter)
+	suite.mockCreatorRepo.(*mocks_repo.MockCreatorRepository).AssertNotCalled(suite.T(), "Save", expectedCreator)
+	suite.mockRFService.(*mocks.MockRFService).AssertNotCalled(
+		suite.T(),
+		"ConsultaCPF",
+		expectedCreator.CPF.String(),
+		expectedCreator.Birthdate.Format("02/01/2006"),
+	)
 }
