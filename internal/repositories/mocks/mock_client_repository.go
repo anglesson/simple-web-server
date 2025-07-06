2a8f0cb99b1cba3f3a432f93df02df3fcc003000
Anglesson Araujo <anglesson@outlook.com>
2025-07-06 11:17:31 -0300

    refactor: move mock to repository mocks package

---
+++ internal/repositories/mocks/mock_client_repository.go
@@ -0,0 +1,49 @@
+package mocks
+
+import (
+	"github.com/anglesson/simple-web-server/domain"
+	"github.com/anglesson/simple-web-server/internal/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockClientRepository struct {
+	mock.Mock
+}
+
+func (m *MockClientRepository) Save(client *models.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func (m *MockClientRepository) FindClientsByCreator(creator *models.Creator, query domain.ClientFilter) (*[]models.Client, error) {
+	args := m.Called(creator, query)
+	return args.Get(0).(*[]models.Client), args.Error(1)
+}
+
+func (m *MockClientRepository) FindByIDAndCreators(client *models.Client, clientID uint, creator string) error {
+	args := m.Called(client, clientID, creator)
+	return args.Error(0)
+}
+
+func (m *MockClientRepository) FindByClientsWhereEbookNotSend(creator *models.Creator, query domain.ClientFilter) (*[]models.Client, error) {
+	args := m.Called(creator, query)
+	return args.Get(0).(*[]models.Client), args.Error(1)
+}
+
+func (m *MockClientRepository) FindByClientsWhereEbookWasSend(creator *models.Creator, query domain.ClientFilter) (*[]models.Client, error) {
+	args := m.Called(creator, query)
+	return args.Get(0).(*[]models.Client), args.Error(1)
+}
+
+func (m *MockClientRepository) InsertBatch(clients []*models.Client) error {
+	args := m.Called(clients)
+	return args.Error(0)
+}
+
+func (m *MockClientRepository) FindByEmail(email string) (*models.Client, error) {
+	args := m.Called(email)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*models.Client), args.Error(1)
+}
--- internal/services/client_service_test.go
+++ internal/services/client_service_test.go
@@ -5,61 +5,17 @@ import (
 	"github.com/anglesson/simple-web-server/pkg/gov/mocks"
 	"testing"
 
-	"github.com/anglesson/simple-web-server/domain"
 	"github.com/anglesson/simple-web-server/internal/repositories"
 	"github.com/anglesson/simple-web-server/internal/services"
 	"github.com/anglesson/simple-web-server/pkg/gov"
 
 	"github.com/anglesson/simple-web-server/internal/models"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
-var _ repositories.ClientRepository = (*MockClientRepository)(nil)
+var _ repositories.ClientRepository = (*mocks_repo.MockClientRepository)(nil)
 var _ repositories.CreatorRepository = (*mocks_repo.MockCreatorRepository)(nil)
 var _ gov.ReceitaFederalService = (*mocks.MockRFService)(nil)
-
-type MockClientRepository struct {
-	mock.Mock
-}
-
-func (m *MockClientRepository) Save(client *models.Client) error {
-	args := m.Called(client)
-	return args.Error(0)
-}
-
-func (m *MockClientRepository) FindClientsByCreator(creator *models.Creator, query domain.ClientFilter) (*[]models.Client, error) {
-	args := m.Called(creator, query)
-	return args.Get(0).(*[]models.Client), args.Error(1)
-}
-
-func (m *MockClientRepository) FindByIDAndCreators(client *models.Client, clientID uint, creator string) error {
-	args := m.Called(client, clientID, creator)
-	return args.Error(0)
-}
-
-func (m *MockClientRepository) FindByClientsWhereEbookNotSend(creator *models.Creator, query domain.ClientFilter) (*[]models.Client, error) {
-	args := m.Called(creator, query)
-	return args.Get(0).(*[]models.Client), args.Error(1)
-}
-
-func (m *MockClientRepository) FindByClientsWhereEbookWasSend(creator *models.Creator, query domain.ClientFilter) (*[]models.Client, error) {
-	args := m.Called(creator, query)
-	return args.Get(0).(*[]models.Client), args.Error(1)
-}
-
-func (m *MockClientRepository) InsertBatch(clients []*models.Client) error {
-	args := m.Called(clients)
-	return args.Error(0)
-}
-
-func (m *MockClientRepository) FindByEmail(email string) (*models.Client, error) {
-	args := m.Called(email)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*models.Client), args.Error(1)
-}
 
 type ClientServiceTestSuite struct {
 	suite.Suite
@@ -70,7 +26,7 @@ type ClientServiceTestSuite struct {
 }
 
 func (suite *ClientServiceTestSuite) SetupTest() {
-	suite.mockClientRepository = new(MockClientRepository)
+	suite.mockClientRepository = new(mocks_repo.MockClientRepository)
 	suite.mockCreatorRepository = new(mocks_repo.MockCreatorRepository)
 	suite.mockRFService = new(mocks.MockRFService)
 	suite.sut = services.NewClientService(suite.mockClientRepository, suite.mockCreatorRepository, suite.mockRFService)
@@ -112,11 +68,11 @@ func (suite *ClientServiceTestSuite) TestCreateClient() {
 		On("FindCreatorByUserEmail", creator.Contact.Email).
 		Return(creator, nil)
 
-	suite.mockClientRepository.(*MockClientRepository).
+	suite.mockClientRepository.(*mocks_repo.MockClientRepository).
 		On("FindByEmail", input.Email).
 		Return(nil, nil)
 
-	suite.mockClientRepository.(*MockClientRepository).
+	suite.mockClientRepository.(*mocks_repo.MockClientRepository).
 		On("Save", client).
 		Return(nil)
 
@@ -124,7 +80,7 @@ func (suite *ClientServiceTestSuite) TestCreateClient() {
 
 	suite.NoError(err)
 	suite.mockCreatorRepository.(*mocks_repo.MockCreatorRepository).AssertCalled(suite.T(), "FindCreatorByUserEmail", creator.Contact.Email)
-	suite.mockClientRepository.(*MockClientRepository).AssertCalled(suite.T(), "Save", client)
+	suite.mockClientRepository.(*mocks_repo.MockClientRepository).AssertCalled(suite.T(), "Save", client)
 }
 
 func (suite *ClientServiceTestSuite) TestShouldReturnErrorIfClientExists() {
@@ -163,7 +119,7 @@ func (suite *ClientServiceTestSuite) TestShouldReturnErrorIfClientExists() {
 		On("FindCreatorByUserEmail", creator.Contact.Email).
 		Return(creator, nil)
 
-	suite.mockClientRepository.(*MockClientRepository).
+	suite.mockClientRepository.(*mocks_repo.MockClientRepository).
 		On("FindByEmail", input.Email).
 		Return(client, nil)
 
@@ -171,7 +127,7 @@ func (suite *ClientServiceTestSuite) TestShouldReturnErrorIfClientExists() {
 
 	suite.Error(err)
 	suite.mockCreatorRepository.(*mocks_repo.MockCreatorRepository).AssertCalled(suite.T(), "FindCreatorByUserEmail", creator.Contact.Email)
-	suite.mockClientRepository.(*MockClientRepository).AssertCalled(suite.T(), "FindByEmail", client.Contact.Email)
+	suite.mockClientRepository.(*mocks_repo.MockClientRepository).AssertCalled(suite.T(), "FindByEmail", client.Contact.Email)
 }
 
 func TestClientServiceTestSuite(t *testing.T) {
