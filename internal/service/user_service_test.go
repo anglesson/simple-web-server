0957b57042a7dbdf06ff0fc623cee9f0ab543fa5
Anglesson Araujo <anglesson@outlook.com>
2025-07-12 17:54:28 -0300

    test: add test case for password confirmation validation in CreateUser

--- internal/service/user_service_test.go
+++ internal/service/user_service_test.go
@@ -32,13 +32,14 @@ func TestUserServiceTestSuite(t *testing.T) {
 func (suite *UserServiceTestSuite) TestCreateUser() {
 	inputUsername := "Any Username"
 	inputPassword := "AnyPassword"
+	inputPasswordConfirmation := "AnyPassword"
 	inputEmail := "any@user.com"
 
 	suite.mockEncrypter.(*utilsMocks.MockEncrypter).On("HashPassword", inputPassword).Return("HashedPassword")
 	expectedUser, _ := domain.NewUser(inputUsername, inputEmail, "HashedPassword")
 
 	suite.mockUserRepository.(*mocks.MockUserRepository).On("Save", &expectedUser).Return(nil)
-	_, err := suite.sut.CreateUser(inputUsername, inputEmail, inputPassword)
+	_, err := suite.sut.CreateUser(inputUsername, inputEmail, inputPassword, inputPasswordConfirmation)
 
 	suite.NoError(err)
 	suite.mockUserRepository.(*mocks.MockUserRepository).AssertCalled(suite.T(), "Save", &expectedUser)
@@ -47,16 +48,29 @@ func (suite *UserServiceTestSuite) TestCreateUser() {
 func (suite *UserServiceTestSuite) TestCreateUserWithHashedPassword() {
 	inputUsername := "Any Username"
 	inputPassword := "AnyPassword"
+	inputPasswordConfirmation := "AnyPassword"
 	inputEmail := "any@user.com"
 
 	expectedUser, _ := domain.NewUser(inputUsername, inputEmail, "HashedPassword")
 
 	suite.mockEncrypter.(*utilsMocks.MockEncrypter).On("HashPassword", inputPassword).Return("HashedPassword")
 	suite.mockUserRepository.(*mocks.MockUserRepository).On("Save", mock.Anything).Return(nil)
-	user, err := suite.sut.CreateUser(inputUsername, inputEmail, inputPassword)
+	user, err := suite.sut.CreateUser(inputUsername, inputEmail, inputPassword, inputPasswordConfirmation)
 
 	suite.NoError(err)
 	suite.mockEncrypter.(*utilsMocks.MockEncrypter).AssertCalled(suite.T(), "HashPassword", "AnyPassword")
 	suite.mockUserRepository.(*mocks.MockUserRepository).AssertCalled(suite.T(), "Save", &expectedUser)
 	suite.Assert().Equal("HashedPassword", user.Password)
+}
+
+func (suite *UserServiceTestSuite) TestShouldReturnErrorIfPasswordAndConfirmationAreDifferent() {
+	inputUsername := "Any Username"
+	inputPassword := "AnyPassword"
+	inputPasswordConfirmation := "AnyPasswordConfirmation"
+	inputEmail := "any@user.com"
+
+	user, err := suite.sut.CreateUser(inputUsername, inputEmail, inputPassword, inputPasswordConfirmation)
+
+	suite.Error(err)
+	suite.Nil(user)
 }
